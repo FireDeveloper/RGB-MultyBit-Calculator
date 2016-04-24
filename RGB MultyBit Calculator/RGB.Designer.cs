5f269ff78f790036884d5a825eec9f15342257d0
Stefanos <FireDeveloper@users.noreply.github.com>
2016-04-24 19:47:37 +0300

    Added files via upload

---
+++ RGB MultyBit Calculator/RGB.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace RGB_MultyBit_Calculator {
+    partial class RGB {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RGB));
+            this.RedBar = new System.Windows.Forms.TrackBar();
+            this.GreenBar = new System.Windows.Forms.TrackBar();
+            this.BlueBar = new System.Windows.Forms.TrackBar();
+            this.RedUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GreenUpDown = new System.Windows.Forms.NumericUpDown();
+            this.BlueUpDown = new System.Windows.Forms.NumericUpDown();
+            this.RGBcolorPanel = new System.Windows.Forms.Panel();
+            this.txtHexBox = new System.Windows.Forms.TextBox();
+            this.RGBUpDown = new System.Windows.Forms.NumericUpDown();
+            this.RGBBar = new System.Windows.Forms.TrackBar();
+            this.HighRGB = new System.Windows.Forms.RadioButton();
+            this.LowRGB = new System.Windows.Forms.RadioButton();
+            this.BtnColor = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBBar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RedBar
+            // 
+            this.RedBar.Location = new System.Drawing.Point(79, 37);
+            this.RedBar.Maximum = 255;
+            this.RedBar.Name = "RedBar";
+            this.RedBar.Size = new System.Drawing.Size(300, 45);
+            this.RedBar.TabIndex = 0;
+            this.RedBar.TickFrequency = 5;
+            this.RedBar.Scroll += new System.EventHandler(this.RedBar_Scroll);
+            // 
+            // GreenBar
+            // 
+            this.GreenBar.Location = new System.Drawing.Point(79, 88);
+            this.GreenBar.Maximum = 255;
+            this.GreenBar.Name = "GreenBar";
+            this.GreenBar.Size = new System.Drawing.Size(300, 45);
+            this.GreenBar.TabIndex = 1;
+            this.GreenBar.TickFrequency = 5;
+            this.GreenBar.Scroll += new System.EventHandler(this.GreenBar_Scroll);
+            // 
+            // BlueBar
+            // 
+            this.BlueBar.Location = new System.Drawing.Point(79, 139);
+            this.BlueBar.Maximum = 255;
+            this.BlueBar.Name = "BlueBar";
+            this.BlueBar.Size = new System.Drawing.Size(300, 45);
+            this.BlueBar.TabIndex = 2;
+            this.BlueBar.TickFrequency = 5;
+            this.BlueBar.Scroll += new System.EventHandler(this.BlueBar_Scroll);
+            // 
+            // RedUpDown
+            // 
+            this.RedUpDown.BackColor = System.Drawing.Color.Red;
+            this.RedUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.RedUpDown.Location = new System.Drawing.Point(12, 37);
+            this.RedUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.RedUpDown.Name = "RedUpDown";
+            this.RedUpDown.Size = new System.Drawing.Size(60, 26);
+            this.RedUpDown.TabIndex = 3;
+            this.RedUpDown.Tag = "RED";
+            this.RedUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.RedUpDown.ValueChanged += new System.EventHandler(this.RedUpDown_ValueChanged);
+            // 
+            // GreenUpDown
+            // 
+            this.GreenUpDown.BackColor = System.Drawing.Color.Green;
+            this.GreenUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.GreenUpDown.Location = new System.Drawing.Point(13, 88);
+            this.GreenUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.GreenUpDown.Name = "GreenUpDown";
+            this.GreenUpDown.Size = new System.Drawing.Size(60, 26);
+            this.GreenUpDown.TabIndex = 4;
+            this.GreenUpDown.Tag = "GREEN";
+            this.GreenUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.GreenUpDown.ValueChanged += new System.EventHandler(this.GreenUpDown_ValueChanged);
+            // 
+            // BlueUpDown
+            // 
+            this.BlueUpDown.BackColor = System.Drawing.Color.Blue;
+            this.BlueUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.BlueUpDown.ForeColor = System.Drawing.Color.White;
+            this.BlueUpDown.Location = new System.Drawing.Point(13, 139);
+            this.BlueUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.BlueUpDown.Name = "BlueUpDown";
+            this.BlueUpDown.Size = new System.Drawing.Size(60, 26);
+            this.BlueUpDown.TabIndex = 5;
+            this.BlueUpDown.Tag = "BLUE";
+            this.BlueUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BlueUpDown.ValueChanged += new System.EventHandler(this.BlueUpDown_ValueChanged);
+            // 
+            // RGBcolorPanel
+            // 
+            this.RGBcolorPanel.Location = new System.Drawing.Point(385, 66);
+            this.RGBcolorPanel.Name = "RGBcolorPanel";
+            this.RGBcolorPanel.Size = new System.Drawing.Size(120, 73);
+            this.RGBcolorPanel.TabIndex = 6;
+            // 
+            // txtHexBox
+            // 
+            this.txtHexBox.BackColor = System.Drawing.Color.White;
+            this.txtHexBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.txtHexBox.Location = new System.Drawing.Point(385, 145);
+            this.txtHexBox.Name = "txtHexBox";
+            this.txtHexBox.ReadOnly = true;
+            this.txtHexBox.Size = new System.Drawing.Size(120, 26);
+            this.txtHexBox.TabIndex = 7;
+            this.txtHexBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // RGBUpDown
+            // 
+            this.RGBUpDown.BackColor = System.Drawing.Color.White;
+            this.RGBUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.RGBUpDown.ForeColor = System.Drawing.Color.Black;
+            this.RGBUpDown.Hexadecimal = true;
+            this.RGBUpDown.Location = new System.Drawing.Point(12, 190);
+            this.RGBUpDown.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.RGBUpDown.Name = "RGBUpDown";
+            this.RGBUpDown.Size = new System.Drawing.Size(60, 23);
+            this.RGBUpDown.TabIndex = 8;
+            this.RGBUpDown.Tag = "RED";
+            this.RGBUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.RGBUpDown.ValueChanged += new System.EventHandler(this.RGBUpDown_ValueChanged);
+            // 
+            // RGBBar
+            // 
+            this.RGBBar.Location = new System.Drawing.Point(79, 190);
+            this.RGBBar.Maximum = 65535;
+            this.RGBBar.Name = "RGBBar";
+            this.RGBBar.Size = new System.Drawing.Size(300, 45);
+            this.RGBBar.TabIndex = 9;
+            this.RGBBar.TickFrequency = 1285;
+            this.RGBBar.Scroll += new System.EventHandler(this.RGBBar_Scroll);
+            // 
+            // HighRGB
+            // 
+            this.HighRGB.AutoSize = true;
+            this.HighRGB.Checked = true;
+            this.HighRGB.Location = new System.Drawing.Point(385, 177);
+            this.HighRGB.Name = "HighRGB";
+            this.HighRGB.Size = new System.Drawing.Size(103, 17);
+            this.HighRGB.TabIndex = 10;
+            this.HighRGB.TabStop = true;
+            this.HighRGB.Text = "Using High RGB";
+            this.HighRGB.UseVisualStyleBackColor = true;
+            // 
+            // LowRGB
+            // 
+            this.LowRGB.AutoSize = true;
+            this.LowRGB.Location = new System.Drawing.Point(385, 200);
+            this.LowRGB.Name = "LowRGB";
+            this.LowRGB.Size = new System.Drawing.Size(101, 17);
+            this.LowRGB.TabIndex = 11;
+            this.LowRGB.Text = "Using Low RGB";
+            this.LowRGB.UseVisualStyleBackColor = true;
+            // 
+            // BtnColor
+            // 
+            this.BtnColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.BtnColor.Location = new System.Drawing.Point(385, 37);
+            this.BtnColor.Name = "BtnColor";
+            this.BtnColor.Size = new System.Drawing.Size(120, 23);
+            this.BtnColor.TabIndex = 12;
+            this.BtnColor.Text = "Colors";
+            this.BtnColor.UseVisualStyleBackColor = true;
+            this.BtnColor.Click += new System.EventHandler(this.BtnColor_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(520, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // RGB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 238);
+            this.Controls.Add(this.BtnColor);
+            this.Controls.Add(this.LowRGB);
+            this.Controls.Add(this.HighRGB);
+            this.Controls.Add(this.RGBBar);
+            this.Controls.Add(this.RGBUpDown);
+            this.Controls.Add(this.txtHexBox);
+            this.Controls.Add(this.RGBcolorPanel);
+            this.Controls.Add(this.BlueUpDown);
+            this.Controls.Add(this.GreenUpDown);
+            this.Controls.Add(this.RedUpDown);
+            this.Controls.Add(this.BlueBar);
+            this.Controls.Add(this.GreenBar);
+            this.Controls.Add(this.RedBar);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "RGB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB Multy Bit Calculator";
+            this.Load += new System.EventHandler(this.RGB_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RedBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBBar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar RedBar;
+        private System.Windows.Forms.TrackBar GreenBar;
+        private System.Windows.Forms.TrackBar BlueBar;
+        private System.Windows.Forms.NumericUpDown RedUpDown;
+        private System.Windows.Forms.NumericUpDown GreenUpDown;
+        private System.Windows.Forms.NumericUpDown BlueUpDown;
+        private System.Windows.Forms.Panel RGBcolorPanel;
+        private System.Windows.Forms.TextBox txtHexBox;
+        private System.Windows.Forms.NumericUpDown RGBUpDown;
+        private System.Windows.Forms.TrackBar RGBBar;
+        private System.Windows.Forms.RadioButton HighRGB;
+        private System.Windows.Forms.RadioButton LowRGB;
+        private System.Windows.Forms.Button BtnColor;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ RGB MultyBit Calculator/RGB.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RGB_MultyBit_Calculator {
+    public partial class RGB : Form {
+        public RGB() {
+            InitializeComponent();
+        }
+
+        byte red = 0;
+        byte green = 0;
+        byte blue = 0;
+
+        private void RGB_Load(object sender, EventArgs e) {
+            // RGBcolorPanel.CreateGraphics();
+            setRGB();
+        }
+
+        private Int32 showHex(byte r, byte b, byte g) {
+            int result = ((r / 8) << 11) | ((b / 4) << 5) | (g / 8);
+            return Convert.ToInt32(result);
+        }
+
+        private void getRGB() {
+            int rgb = RGBBar.Value;
+            blue = Convert.ToByte((rgb & 0x1F) << 3);
+            green = Convert.ToByte(((rgb >> 5) & 0x3F) << 2);
+            red = Convert.ToByte(((rgb >> 11) & 0x1F) << 3);
+
+            if (HighRGB.Checked) {
+                blue = Convert.ToByte(blue | 0x7);
+                green = Convert.ToByte(green | 0x3);
+                red = Convert.ToByte(red | 0x7);
+            }
+
+            BlueBar.Value = blue;
+            GreenBar.Value = green;
+            RedBar.Value = red;
+
+            RedUpDown.Value = red;
+            GreenUpDown.Value = green;
+            BlueUpDown.Value = blue;
+
+            txtHexBox.Text = "0x" + showHex(red, green, blue).ToString("X").PadLeft(4, '0');
+            RGBcolorPanel.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void setRGB() {
+
+            red = Convert.ToByte(RedBar.Value);
+            green = Convert.ToByte(GreenBar.Value);
+            blue = Convert.ToByte(BlueBar.Value);
+
+            RedUpDown.Value = red;
+            GreenUpDown.Value = green;
+            BlueUpDown.Value = blue;
+            RGBUpDown.Value = showHex(red, green, blue);
+
+            txtHexBox.Text = "0x" + showHex(red, green, blue).ToString("X").PadLeft(4, '0');
+            RGBcolorPanel.BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void RedBar_Scroll(object sender, EventArgs e) {
+            setRGB();
+        }
+
+        private void GreenBar_Scroll(object sender, EventArgs e) {
+            setRGB();
+        }
+
+        private void BlueBar_Scroll(object sender, EventArgs e) {
+            setRGB();
+        }
+
+        private void RGBBar_Scroll(object sender, EventArgs e) {
+            getRGB();
+        }
+
+        private void RedUpDown_ValueChanged(object sender, EventArgs e) {
+            RedBar.Value = Convert.ToInt32(RedUpDown.Value);
+            setRGB();
+        }
+
+        private void GreenUpDown_ValueChanged(object sender, EventArgs e) {
+            GreenBar.Value = Convert.ToInt32(GreenUpDown.Value);
+            setRGB();
+        }
+
+        private void BlueUpDown_ValueChanged(object sender, EventArgs e) {
+            BlueBar.Value = Convert.ToInt32(BlueUpDown.Value);
+            setRGB();
+        }
+
+        private void RGBUpDown_ValueChanged(object sender, EventArgs e) {
+            RGBBar.Value = Convert.ToInt32(RGBUpDown.Value);
+            getRGB();
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e) {
+            ColorDialog d = new ColorDialog();
+            if (d.ShowDialog() == DialogResult.OK) {
+                RedBar.Value = d.Color.R;
+                GreenBar.Value = d.Color.G;
+                BlueBar.Value = d.Color.B;
+                setRGB();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+    }
+}
